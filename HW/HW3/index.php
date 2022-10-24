f0e5556e78932f5758947e49cc5ec9f6b629d6c5
Jason Huynh <63384364+TheNMEArengee@users.noreply.github.com>
2022-10-23 19:34:56 -0700

    namespaces and require onces

--- HW/HW3/index.php
+++ HW/HW3/index.php
@@ -7,11 +7,20 @@
     use group\hw3\views\helpers\Helpers;
 
 
-    // For some reason this controller is not being found,
-    // so this method is not quite working
+    require_once("src\controllers\controller.php");
+    require_once("src\models\model.php");
+
+    // These three require onces don't work for whatever reason
+
+    // require_once("src\views\elements\elements.php");
+    // require_once("src\views\helpers\helpers.php");
+    // require_once("src\views\layouts\layouts.php");
+
+
     $controller = new Controller();
     $controller.mainController();
 
 
     echo "Aloha";
+    
 ?>
