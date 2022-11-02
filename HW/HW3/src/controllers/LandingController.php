6676aa3aad04c94a4ccdb85f40c175ffcb855080
LySabrina <lysabrinak@gmail.com>
2022-11-02 00:34:53 -0700

    Update controller

--- HW/HW3/src/controllers/LandingController.php
+++ HW/HW3/src/controllers/LandingController.php
@@ -16,23 +16,20 @@ class LandingController implements Controller{
     function processRequest()
     {
         $model = new Model();     
-        $allPolicyTypes = $model->getAllPolicyType(); //[Lair, Test]
-        $allPolicies = $model->getAllPolicy();          //coffine relocation 
+        $allPolicyTypes = $model->getAllPolicyType(); 
+        $allPolicies = $model->getAllPolicy();          
 
-        $noPolicyArr = [];                             // arrau of policy types that do not have any policues
+        $noPolicyArr = [];                            
         $j = 0;
         for($i = 0 ; $i < count($allPolicyTypes); $i++){
-            // echo($allPolicyTypes[$i]['policyTypeName']);            //LAIR 
+            
             
             $arr = $model->getAllPolicyNamesFromPolicyTypeAsArray($allPolicyTypes[$i]['policyTypeName']);
             if(empty($arr)){
                 $noPolicyArr[$j] = $allPolicyTypes[$i]['policyTypeName'];
                 $j++;
             }
-            // if(($this->hasPolicies($allPolicyTypes[$i]['policyTypeName'])) == false){
-                
-            //     $noPolicyArr[$i] =  $allPolicyTypes[$i]  ;
-            // }
+        
         }
        
         $view = new LandingPage();   
