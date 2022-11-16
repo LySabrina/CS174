529b4a5ffff532ffe97883a4f08038becb275460
Jason Huynh <63384364+TheNMEArengee@users.noreply.github.com>
2022-11-15 22:13:34 -0800

    Update Quizmaker.php

--- HW/HW4/src/Quizmaker.php
+++ HW/HW4/src/Quizmaker.php
@@ -6,10 +6,38 @@ $words = [];                                    //word array to hold all the wor
 
 foreach($files as $f){
     $contents = file_get_contents($f);
-    $string = preg_replace('/[^a-zA-Z]/', ' ', $contents);
+    $string = str_replace(['?', '.', '!'], "", $contents);
     echo($string);
-    $temp = explode(" ", $string);
-    $words = array_merge($words, $temp);
+    $rawTextArray = explode(" ", $string);
+    // print_r($rawTextArray);
+    $textArrayLowercase = array_map('strtolower', $rawTextArray);
+    // print_r($textArrayLowercase);
+    $allUniqueWords = array_unique($textArrayLowercase);
+    // print_r($allUniqueWords);
+
+
+    $length = count($textArrayLowercase);
+
+
+    foreach($allUniqueWords as $word){
+        $fivegram = [];
+        for($a = 0; $a < $length; $a++){
+            if($textArrayLowercase[$a] == $word){
+                for($b = $a - 2; $b <= $a + 2; $b++){
+                    if($b < 0 || $b >= $length){
+                        array_push($fivegram, "Null");
+                    }
+                    else{
+                        array_push($fivegram, $textArrayLowercase[$b]);
+                    }
+                }
+            }
+        }
+        print_r($fivegram);
+    }
+   
+    
+    $words = array_merge($words, $rawTextArray);
 }
 $results = array_count_values(array_map("strtolower", $words));
 unset($results['']);
