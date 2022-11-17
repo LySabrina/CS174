c15ad35a32f188d1791a827833a0665948739206
LySabrina <lysabrinak@gmail.com>
2022-11-16 20:37:58 -0800

    Updated code

---
+++ HW/HW4/src/Controllers/QuizController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace group\hw4\Controllers;
+class QuizController{
+    function __construct()
+    {
+        
+    }
+
+    function getQuiz(){
+        $model = new QuizModel();
+    }
+}
