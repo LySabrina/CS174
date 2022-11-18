29629f2f23c4ff31e2cdbb47a976d08242a641d3
LySabrina <lysabrinak@gmail.com>
2022-11-18 00:34:20 -0800

    Fix quizMaker.php

--- HW/HW4/src/Quizmaker.php
+++ HW/HW4/src/Quizmaker.php
@@ -1,115 +1,80 @@
-<!-- <?php
+<?php
 
-$files = glob("data/*",GLOB_ONLYDIR );      //gets an array of the subdirectories (e.xenglish, spanish)
+
+declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+
+$logger = new Logger('info');
+$logger->pushHandler(new StreamHandler(__DIR__.'/output.log', Logger::DEBUG));
+$logger->info('Quizmaker Processing Start');
+
+
+$files = glob("data/*",GLOB_ONLYDIR );      //gets an array of the subdirectories (e.xenglish, spanish)
+//O(n^4)....
 foreach($files as $f){
+
+    $logger->info("Processing Folder: ", [$f]);
+
     echo("DIRECTORY: " . $f);
     //data/english
     $texts = glob($f ."/*.txt"); //data/english/fox.txt, data/english/zebra.txt
     
     foreach($texts as $t){
         
+        $logger->info("Processing File:", [$t]);
+
         $contents = file_get_contents($t);
-        $string = str_replace(['?', '.', '!', ','], "", $contents);
-            $rawTextArray = explode(" ", $string);
-    // print_r($rawTextArray);
-    $textArrayLowercase = array_map('strtolower', $rawTextArray);
+        $string = str_replace(['?', '.', '!', ',','(',')','[',']','{','}','-','_',':',';','"',], "", $contents);
+        $rawTextArray = explode(" ", $string);
+        $textArrayLowercase = array_map('strtolower', $rawTextArray);
         $allUniqueWords = array_unique($textArrayLowercase);
         $length = count($textArrayLowercase);
+
         foreach($allUniqueWords as $word){
-                    $fivegram = [];
-                    for($a = 0; $a < $length; $a++){
-                        if($textArrayLowercase[$a] == $word){
-                            for($b = $a - 2; $b <= $a + 2; $b++){
-                                if($b < 0 || $b >= $length){
-                                    array_push($fivegram, "Null");
-                                }
-                                else{
-                                    array_push($fivegram, $textArrayLowercase[$b]);
-                                }
-                            }
+            $fivegram = [];
+            for($a = 0; $a < $length; $a++){
+                if($textArrayLowercase[$a] == $word){
+                    for($b = $a - 2; $b <= $a + 2; $b++){
+                        if($b < 0 || $b >= $length){
+                            array_push($fivegram, "Null");
+                        }
+                        else{
+                            array_push($fivegram, $textArrayLowercase[$b]);
                         }
                     }
-                    
-                    $num = count($fivegram) / 5;
-                    $associatedWords[$word] = array($num, $fivegram);
-                    
-                        uasort($associatedWords, function($a, $b){
-                            return $b[0] - $a[0]  ;
-                        });
-                    
-                    // print_r($fivegram);
                 }
-                // $words = array_merge($words, $rawTextArray);    //dont need?
             }
-        
-        echo("HERE IS THE NAME: " . $t . "\n");
-        $type = explode("/", $f);
-        $myFile = fopen("data/" . $type[1] . ".txt", "w");
+                    
+            $num = count($fivegram) / 5;
+            $associatedWords[$word] = array($num, $fivegram);
+                    
+            uasort($associatedWords, function($a, $b){
+                return $b[0] - $a[0]  ;
+            });
+        }
 
-        fwrite($myFile, serialize($associatedWords));
-        print_r($associatedWords);
-        unset($associatedWords);    
+        $logger->info("Finished File");
+        
+    }
+        
+    echo("HERE IS THE NAME: " . $t . "\n");
+    $type = explode("/", $f);
+    $myFile = fopen("data/" . $type[1] . ".txt", "w");
+
+    fwrite($myFile, serialize($associatedWords));
+    print_r($associatedWords);
+    unset($associatedWords);
+
+    $logger->info("Finished Folder");
+        
 }
-
-
-// $files = glob("data/english/*.txt");            //returns an array of .txt files
-
-
-// $words = [];                                    //word array to hold all the words found inside the text file
-// $associatedWords = [];                          //
-// foreach($files as $f){
-//     $contents = file_get_contents($f);
-//     $string = str_replace(['?', '.', '!', ','], "", $contents);
-//     // echo($string);
-//     $rawTextArray = explode(" ", $string);
-//     // print_r($rawTextArray);
-//     $textArrayLowercase = array_map('strtolower', $rawTextArray);
-//     // print_r($textArrayLowercase);
-//     $allUniqueWords = array_unique($textArrayLowercase);
-//     // print_r($allUniqueWords);
-
-
-//     $length = count($textArrayLowercase);
-
-
-//     foreach($allUniqueWords as $word){
-//         $fivegram = [];
-//         for($a = 0; $a < $length; $a++){
-//             if($textArrayLowercase[$a] == $word){
-//                 for($b = $a - 2; $b <= $a + 2; $b++){
-//                     if($b < 0 || $b >= $length){
-//                         array_push($fivegram, "Null");
-//                     }
-//                     else{
-//                         array_push($fivegram, $textArrayLowercase[$b]);
-//                     }
-//                 }
-//             }
-//         }
-//         //['the' => fiveGram]
-//         $num = count($fivegram) / 5;
-//         $associatedWords[$word] = array($num, $fivegram);
-        
-//         // print_r($fivegram);
-//     }
-//     $words = array_merge($words, $rawTextArray);    //dont need?
-// }
-
-
-// // //How to order from most occuring to last
-// // usort($associatedWords, function($a, $b){
-// //     return $b[0] - $a[0]  ;
-// // });
-
-
-// // $myFile = fopen("englishQuiz.txt", )
-// // print("<-------------- HERE ------------------->");
-
-// // foreach(glob("data/*", GLOB_ONLYDIR) as $f){
-// //     echo(substr($f, 5) . "\n");
-// // }
-// // $results = array_count_values(array_map("strtolower", $words));
-// // print_r($results);
-// print_r($associatedWords); -->
